c8214c6b50d0db32f5031edca1643186dc500ada
Michael Blinn <michael.blinn@giv2giv.org>
2014-08-21 22:46:25 -0400

    Building project

--- Gruntfile.js
+++ Gruntfile.js
@@ -150,7 +150,7 @@ module.exports = function (grunt) {
         imagesDir: '<%= yeoman.app %>/images',
         javascriptsDir: '<%= yeoman.app %>/scripts',
         fontsDir: '<%= yeoman.app %>/styles/fonts',
-        importPath: '<%= yeoman.app %>/bower_components',
+        importPath: 'bower_components',
         httpImagesPath: '/images',
         httpGeneratedImagesPath: '/images/generated',
         httpFontsPath: '/styles/fonts',
